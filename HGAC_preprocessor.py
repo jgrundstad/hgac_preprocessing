e1b6ebe17ff72d759d592c2b24dcb58b825b8b86
Jason Grundstad <jgrundstad@gmail.com>
2015-10-29 13:53:37 -0500

    find unprocessed runs testing

--- HGAC_preprocessor.py
+++ HGAC_preprocessor.py
@@ -1,19 +1,40 @@
 import argparse
+import glob
+import json
 import os
+
 __author__ = 'jgrundst'
+
+config = dict()
 
 
 def find_unprocessed(root_dir=None):
-    pass
+    global config
+    transferred_runs = [x.split('/')[3] for x in glob.glob(
+        os.path.join(root_dir, '*', config['transfer_complete_filename']))]
+    processed_runs = [x.split('/')[3] for x in glob.glob(
+        os.path.join(root_dir, '*', config['processing_complete_filename']))]
+    return list(set(transferred_runs) - set(processed_runs))
+
+
+def parse_config(file=None):
+    global config
+    with open(file) as f:
+        data = f.read()
+    config = json.loads(data)
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Scan for new Illumina runs." +
-                                     " Fire off preprocessing/demultiplexing.")
+    global config
+    parser = argparse.ArgumentParser(description='Scan for new Illumina runs. ' +
+                                     'Fire off preprocessing/demultiplexing.')
     parser.add_argument('-c', '--config', dest='config_file',
                         help='Config file (.json)', required=True)
     args = parser.parse_args()
-    print "Config file {}".format(args.config_file)
+
+    parse_config(file=args.config_file)
+    unprocessed_runs = find_unprocessed(root_dir=config['root_dir'])
+    print unprocessed_runs
 
 
 if __name__ == '__main__':
