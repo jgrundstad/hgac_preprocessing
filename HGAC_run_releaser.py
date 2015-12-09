c4aa17806b09aeab24e0e10952b288d45a6734e6
Jason Grundstad <jgrundstad@gmail.com>
2015-12-09 13:30:06 -0600

    fleshing out HGAC_run_releaser

--- HGAC_run_releaser.py
+++ HGAC_run_releaser.py
@@ -1,6 +1,18 @@
 import argparse
 
+from HGAC_run_monitor import parse_config
+
 __author__ = 'A. Jason Grundstad'
+
+
+class Releaser():
+
+    def __init__(self, config_file=None):
+        self.config = parse_config(config_file=config_file)
+
+
+    def find_completed(self):
+        pass
 
 
 def main():
@@ -8,6 +20,11 @@ def main():
     parser.add_argument('-c', '--config', desc='config_file', required=True,
                         help='Config file (.json)')
     args = parser.parse_args()
+
+    r = Releaser(config_file=args.config)
+
+
+
 
 
 if __name__ == '__main__':
